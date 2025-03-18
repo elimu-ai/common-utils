3208293650ee3f3efa33d9d4c02a8b87bb8e3bfb
Tuan Nguyen <tuancoltech@gmail.com>
2025-03-18 15:35:46 +0700

    Add KDoc for BundleExt

--- utils/src/main/java/ai/elimu/common/utils/BundleExt.kt
+++ utils/src/main/java/ai/elimu/common/utils/BundleExt.kt
@@ -3,6 +3,13 @@ package ai.elimu.common.utils
 import android.os.Build
 import android.os.Bundle
 
+/**
+ * Gets a parcelable object from the Bundle in a way that's compatible with all Android API levels.
+ *
+ * @param key The key to retrieve the parcelable object.
+ * @param clazz The class of the parcelable object.
+ * @return The parcelable object, or null if not found.
+ */
 fun <T> Bundle?.getParcelableCompat(key: String, clazz: Class<T>): T? {
     return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
         this?.getParcelable(key, clazz)
@@ -12,6 +19,13 @@ fun <T> Bundle?.getParcelableCompat(key: String, clazz: Class<T>): T? {
     }
 }
 
+/**
+ * Gets an ArrayList of parcelable objects from the Bundle in a way that's compatible with all Android API levels.
+ *
+ * @param key The key to retrieve the ArrayList.
+ * @param clazz The class of the parcelable objects in the ArrayList.
+ * @return The ArrayList of parcelable objects, or null if not found.
+ */
 fun <T : android.os.Parcelable> Bundle?.getParcelableArrayListCompat(key: String, clazz: Class<T>)
         : java.util.ArrayList<T>? {
     return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
